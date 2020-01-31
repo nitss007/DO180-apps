4a9d1b4a65697976283170396ce8f9c74ad6ff1b
Student User <student@workstation.lab.example.com>
2020-01-31 15:32:57 +0000

    changed front page

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,11 @@
-<?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-?>
+ <html>
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
